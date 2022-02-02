9e8cc021e6177933df61a5a24aa5049ae232140b
TimothéeLVB <timLvb@users.noreply.github.com>
2022-02-02 18:46:20 +0100

    Add folder SRC

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const bruteForce = require('./bruteForce');
-const wordGenerator = require('./wordGenerator');
+const bruteForce = require('./src/bruteForce');
+const wordGenerator = require('./src/wordGenerator');
 
 (async () => {
   wordGenerator.configurePlage(false, false, true, false, ["[", "\\", "]", "^", "_"]); // "`", "{", "|", "}", "~" ne sont pas reconnu comme identique même en comparant les charcode... wtf
